b4a9fb51ec7cbabf475bc6bf60a8eb894232ece2
1903494 <1903494@01-19834.pusd.dom>
2022-10-24 08:48:42 -0700

    Classes project.

---
+++ Classes/src/Runner.java
@@ -0,0 +1,16 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		double b;
+		String c;
+		boolean d;
+		
+		System.out.println(Dog.feetToMeters(50));		
+		
+	}
+
+}
