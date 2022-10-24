4030963d8d37d0303f2b3811d425e6230ad1e338
1903494 <1903494@01-19834.pusd.dom>
2022-10-24 08:51:31 -0700

    Friday project.

---
+++ Friday Project/src/Driver.java
@@ -0,0 +1,21 @@
+
+public class Driver {
+
+	public static void main(String[] args) {	//opening curly -- code below
+		
+		//this is the "starting" point of the project
+		//code below will execute
+		/*
+		 * I love my mother
+		 * I also love my father
+		 * They are family
+		 */
+		
+		//case matters in Java!
+		//red underline means incorrect syntax
+		System.out.println("Hello World!");
+		System.out.println("Happy Friday!");
+
+	}	//closing curly -- add code above
+
+}
---
+++ Friday Project/src/Driver2.java
@@ -0,0 +1,58 @@
+
+public class Driver2 {
+
+	public static void main(String[] args) {
+		
+		//primitive types - variables
+		//only stores one value at a time
+		/*
+		 * syntax:
+		 * 		type name;
+		 */
+		int x;  // declare a variable x to hold ints
+		
+		//assignment statement
+		x = 13;
+//		System.out.println(x);
+//		System.out.print(x);
+//		System.out.print(x);
+//		System.out.println(x);
+//		System.out.print(x);
+//		System.out.println(x);
+//		System.out.print(x);
+		
+		//declare AND assign
+		int y = 5;  //initializes the value at the same time
+		
+		//simple arithmetic in java
+		System.out.println(x + y);
+		System.out.println(x + "+" + y);
+		
+		//find the product x multiplied to y
+		System.out.println(x * y);
+		
+		//x divided by y
+		System.out.println(x / y);
+		
+		//remainder
+		//what is the remainder after dividing x by y
+		System.out.println(x%y);
+		System.out.println(15%3);
+		System.out.println(15%7);
+		System.out.println(7%15);
+		
+		//x to the power of y
+		System.out.println(Math.pow(x , y));
+		
+		//Decimal values
+		//double
+		int		myX = 3;
+		double 	myY = 3.5;
+		
+		//true or false values
+		//boolean
+		boolean isFriday = true;
+
+	}
+
+}
